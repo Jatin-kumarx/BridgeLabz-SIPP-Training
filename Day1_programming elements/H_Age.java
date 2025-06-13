aabd23763b1470a57f1cb10b892b707e098b112d
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-13 16:47:44 +0530

    [Jatin Kumar] Add . H_Age

---
+++ Day1_programming elements/H_Age.java
@@ -0,0 +1,8 @@
+public class H_Age{
+    public static void main(String[] args){
+        int current_yr = 2024;
+        int birth_yr = 2000;
+        int age = current_yr-birth_yr;
+        System.out.println("Age is" +  age);
+    }
+}
