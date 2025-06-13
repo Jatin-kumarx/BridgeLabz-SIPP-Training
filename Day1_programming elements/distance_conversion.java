6332ad67432752647818b71f7e0b093293fc7ee9
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-13 16:46:10 +0530

    [Jatin Kumar] Add . distance_conversion

---
+++ Day1_programming elements/distance_conversion.java
@@ -0,0 +1,9 @@
+public class distance_conversion {
+    public static void main(String[] args){
+        double km = 10.8;
+        double miles = km*1.6;
+        System.out.println("the distance in  miles is " + miles);
+
+    }
+    
+}
