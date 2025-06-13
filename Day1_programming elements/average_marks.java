e644b5b6f7e6bb3006a32073cce19cc34d25a0cb
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-13 16:25:21 +0530

    [Jatin Kumar] Add . average_marks

---
+++ Day1_programming elements/average_marks.java
@@ -0,0 +1,10 @@
+public class average_marks {
+    public static void main(String[]args){
+      int  M_marks= 94;
+      int  C_marks= 96;
+      int  P_marks= 95;
+      int avg_marks = (M_marks + C_marks + P_marks)/3;
+       System.out.println("avg_marks= " + avg_marks);
+    }
+    
+}
