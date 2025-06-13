74b74065376079770edfa7fd02025cbaa539948a
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-13 16:48:18 +0530

    [Jatin Kumar] Add . profit

---
+++ Day1_programming elements/profit.java
@@ -0,0 +1,10 @@
+public class profit {
+    public static void main(String[] args){
+        int SP = 191;
+        int CP = 129;
+        int Profit = SP-CP;
+        double profit_percent = Profit / CP * 100;
+        System.out.println("profit_percent is "+ profit_percent +"%");
+    }
+}
+
