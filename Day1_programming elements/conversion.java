e0d86c618099ebd1c8c790ac0e49c98fe100d7a1
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-13 16:30:57 +0530

    [Jatin Kumar] Add . conversion

---
+++ Day1_programming elements/conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class conversion {
+    public static void main(String[] args) {
+        double km;
+        Scanner sc = new Scanner(System.in);
+        km = sc.nextInt();
+        double miles = km * 0.621371;
+        System.out.println(km + " km is equal to " + miles + " miles");
+
+    
+    }
+}
