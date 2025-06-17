5e91decc09ea10f672d4f08d04e75e9a7986f440
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-17 20:45:45 +0530

    [ jatin kumar ] Add . Firstsmallest

---
+++ Day2_Control Flow/Firstsmallest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Firstsmallest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int number1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int number2 = scanner.nextInt();
+        System.out.print("Enter the third number: ");
+        int number3 = scanner.nextInt();
+        boolean isSmallest = number1 < number2 && number1 < number3;
+        System.out.println("output :  " + (isSmallest ? "Yes" : "No"));
+    }
+}
