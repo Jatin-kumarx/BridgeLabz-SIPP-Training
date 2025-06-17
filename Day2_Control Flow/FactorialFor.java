94ad625220da6fba3dd410d4eddbfd4f12e84620
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-17 20:43:36 +0530

    [ jatin kumar ] Add . FactorialFor

---
+++ Day2_Control Flow/FactorialFor.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FactorialFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        if (n < 0) {
+            System.out.println("Invalid input.");
+            return;
+        }
+        int fact = 1;
+        for (int i = 1; i <= n; i++) fact *= i;
+        System.out.println("Factorial of " + n + " is " + fact);
+    }
+}
