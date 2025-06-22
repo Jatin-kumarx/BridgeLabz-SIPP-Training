6c7c28246be57a6d80d628a9bb47c9f6341c5c51
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-22 20:02:41 +0530

    [ jatin kumar ] Add . NullPointerDemo

---
+++ Day5_Strings/NullPointerDemo.java
@@ -0,0 +1,17 @@
+public class NullPointerDemo {
+    public static void generateNPE() {
+        String text = null;
+        System.out.println(text.length());  // Will throw NullPointerException
+    }
+    public static void handleNPE() {
+        String text = null;
+        try {
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException: " + e.getMessage());
+        }
+    }
+    public static void main(String[] args) {
+        handleNPE(); 
+    }
+}
