cb1587ea932f770b54a9f88a648e6b8d42c68e7b
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-22 21:59:40 +0530

    [ jatin kumar ] Add . FibonacciSequence

---
+++ EXTRAS-built_in_functions/FibonacciSequence.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int terms = sc.nextInt();
+        printFibonacci(terms);
+    }
+    static void printFibonacci(int n) {
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Sequence: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+    }
+}
