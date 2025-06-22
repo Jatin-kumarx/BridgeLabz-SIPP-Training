9303c46b8f64ba79f9aa52784d425d8552675501
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-22 13:20:46 +0530

    [ jatin kumar ] Add . SmallestDigit

---
+++ Day4_Methods/SmallestDigit.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SmallestDigit {
+    public static int findSmallestDigit(int number) {
+        int min = 9;
+        while (number != 0) {
+            int digit = number % 10;
+            if (digit < min) {
+                min = digit;
+            }
+            number /= 10;
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Smallest digit: " + findSmallestDigit(num));
+    }
+}
