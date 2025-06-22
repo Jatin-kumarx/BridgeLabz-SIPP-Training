be0118d81bca6d328c27e316d6284f081fd4300d
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-22 13:19:27 +0530

    [ jatin kumar ] Add . PrimeCheck

---
+++ Day4_Methods/PrimeCheck.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class PrimeCheck {
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+        System.out.println(num + (isPrime(num) ? " is a Prime Number" : " is not a Prime Number"));
+    }
+}
