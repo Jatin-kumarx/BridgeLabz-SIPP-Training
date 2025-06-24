15fbfa2f466229702c4048e14fd9d3c3d423d570
Jatin Kumar <jatingangwar326@gmail.com>
2025-06-24 21:57:12 +0530

    [ jatin kumar ] Add . MobilePhone

---
+++ Day1_Java Class And Object/MobilePhone.java
@@ -0,0 +1,20 @@
+public class MobilePhone {
+    String brand;
+    String model;
+    double price;
+    MobilePhone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+    void displayDetails() {
+        System.out.println("Brand : " + brand);
+        System.out.println("Model : " + model);
+        System.out.println("Price : ₹" + price);
+        System.out.println("---------------------------");
+    }
+    public static void main(String[] args) {
+        MobilePhone phone1 = new MobilePhone("Samsung", "Galaxy A15", 15999);
+        phone1.displayDetails();
+    }
+}
