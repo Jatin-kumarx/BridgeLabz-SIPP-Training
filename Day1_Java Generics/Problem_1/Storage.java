dfd539259fe91a6b3f1349e2a9708792fb312631
Jatin Kumar <jatingangwar326@gmail.com>
2025-07-23 21:45:51 +0530

    [Jatin Kumar] Add . Storage

---
+++ Day1_Java Generics/Problem_1/Storage.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Storage<T extends WarehouseItem> {
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
+    public static void displayItems(List<? extends WarehouseItem> items) {
+        for(WarehouseItem item : items) {
+            System.out.println(item.getName() + " - Rs." + item.getPrice());
+        }
+    }
+}
