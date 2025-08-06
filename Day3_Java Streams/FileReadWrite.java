5849cb636aa505f5a770036c751841efbae48e77
Jatin Kumar <jatingangwar326@gmail.com>
2025-08-06 20:34:33 +0530

    [Jatin Kumar] Add . FileReadWrite

---
+++ Day3_Java Streams/FileReadWrite.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class FileReadWrite {
+    public static void main(String[] args) {
+        File inputFile = new File("source.txt");
+        File outputFile = new File("destination.txt");
+        try (FileInputStream fis = new FileInputStream(inputFile);
+             FileOutputStream fos = new FileOutputStream(outputFile)) {
+            int data;
+            while ((data = fis.read()) != -1) {
+                fos.write(data);
+            }
+            System.out.println("File copied successfully.");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
