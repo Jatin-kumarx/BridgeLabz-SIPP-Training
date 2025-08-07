96594f93d8d7db6081d1c81f1b01df3981da6ac5
Jatin Kumar <jatingangwar326@gmail.com>
2025-08-07 11:03:08 +0530

    [Jatin Kumar] Add . BankSystem

---
+++ Day4_Exceptions/BankSystem.java
@@ -0,0 +1,35 @@
+class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Invalid amount!");
+        }
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance!");
+        }
+        balance -= amount;
+        System.out.println("Withdrawal successful, new balance: " + balance);
+    }
+}
+
+public class BankSystem {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1000);
+        try {
+            account.withdraw(1200);
+        } catch (IllegalArgumentException | InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
